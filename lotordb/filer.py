cb137a0761e034a724d0e91ab026991474a35ae1
Nicklas Boman <smurfd@gmail.com>
2024-03-04 22:30:14 +0100

    packed

--- lotordb/filer.py
+++ lotordb/filer.py
@@ -61,17 +61,14 @@ class LotordbFiler:
     dbsint = int.from_bytes(dbs, 'big') + mnr
     return binascii.hexlify(dbsint.to_bytes(len(dbs), 'big'))
 
-  def add_db_to_file(self, fn, db_name) -> None:
-    r1 = Row(2, 0, [66, 55, 'stuff', 'm0reStUFf', 1234667])
-    t1 = Table(3, 0, ['tb1', 'tb2', 'tb3', 'tb4'], r1)
-    self.pack_db(Db(6, 4, 'db4', 3, ['tb1', 'tb2', 'tb3'], '.lib/db.dbhdr', t1))
+  def add_db_to_file(self, db) -> None:
+    # r1 = Row(2, 0, [66, 55, 'stuff', 'm0reStUFf', 1234667])
+    # t1 = Table(3, 0, ['tb1', 'tb2', 'tb3', 'tb4'], r1)
+    self.pack_db(db)
 
   def pack_write_to_file(self, packed) -> None:
-    print('P=', packed, self.f)
     if self.f:
       [self.f.write(p) for p in packed]
-      # for p in packed:
-      #  self.f.write(p)
 
   def pack_db(self, db) -> None:
     packed: List[Union[bytes, None]] = [None] * 6
@@ -81,21 +78,27 @@ class LotordbFiler:
     packed[3] = struct.pack('>Q', db.nrtbl)
     packed[4] = ','.join(db.tblnames).encode('UTF-8')
     packed[5] = struct.pack('>%ds' % len(db.fn), bytes(db.fn, 'UTF-8'))
-    print('F=', self.f)
     self.pack_write_to_file(packed)
 
   def pack_tbl(self, tbl) -> None:
-    packed: List[Union[bytes, None]] = [None] * 4
+    packed: List[Union[bytes, None]] = [None] * 3
     packed[0] = struct.pack('>Q', tbl.nrtbls)
     packed[1] = struct.pack('>Q', tbl.nrtbl)
-    packed[2] = struct.pack('>%ds' % len(tbl.colnames), bytes(tbl.colnames, 'UTF-8'))
+    packed[2] = ','.join(tbl.colnames).encode('UTF-8')
     self.pack_write_to_file(packed)
 
   def pack_row(self, col) -> None:
-    packed: List[Union[bytes, None]] = [None] * 3
+    packed: List[Union[List[Any], bytes, None]] = [None] * 3
     packed[0] = struct.pack('>Q', col.nrrows)
     packed[1] = struct.pack('>Q', col.nrrow)
-    packed[2] = struct.pack('>%ds' % len(col.data), bytes(col.data, 'UTF-8'))
+    packed[2] = []
+    if isinstance(packed[2], list):
+      for d in col.data:
+        if isinstance(d, str):
+          packed[2].extend(struct.pack('>%ds' % len(d), bytes(d, 'UTF-8')))
+        elif isinstance(d, int):
+          packed[2].extend(struct.pack('>Q', d))
+      packed[2] = bytes(packed[2])
     self.pack_write_to_file(packed)
 
   def add_table_to_db(self, tbl) -> None:
@@ -133,14 +136,18 @@ if __name__ == '__main__':
   d4 = Db(6, 4, 'db4', 3, ['tb1', 'tb2', 'tb3'], '.lib/db.dbhdr', t1)
   d5 = Db(6, 5, 'db5', 3, ['tb1', 'tb2', 'tb3'], '.lib/db.dbhdr', t1)
   d6 = Db(6, 6, 'db6', 3, ['tb1', 'tb2', 'tb3'], '.lib/db.dbhdr', t1)
-  print(d1, d2, d3, d4, d5, d6)
+  print(d1, d2, d3, d4, d5, d6, t1)
 
   db = LotordbFiler()
   db.create_db_file('.lib/db1.db')
-  print('DBF', db.f)
-  db.add_db_to_file('.lib/db1.db', 'db1')
-  db.add_db_to_file('.lib/db1.db', 'db2')
-  db.add_db_to_file('.lib/db1.db', 'db3')
+  db.add_db_to_file(d1)
+  db.add_db_to_file(d2)
+  db.add_db_to_file(d3)
+  db.add_db_to_file(d4)
+  db.add_db_to_file(d5)
+  db.add_db_to_file(d6)
+  db.add_table_to_db(t1)
+  db.add_row_to_table(r1)
   db.close_file()
 
 
