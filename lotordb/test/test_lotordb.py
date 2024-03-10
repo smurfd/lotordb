fcc52ecb3e7d61064420321ce1803f6d21aaefad
Nicklas Boman <smurfd@gmail.com>
2024-03-10 21:33:18 +0100

    test change

--- lotordb/test/test_lotordb.py
+++ lotordb/test/test_lotordb.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 from lotordb.server import LotordbServer, LotordbServerRunnable
 from lotordb.client import LotordbClient
-from lotordb.filer import LotordbFiler
+
+# from lotordb.files import LotordbFile
 import time
 
 
 def test_lotordb() -> None:
   t = time.perf_counter()
-  LotordbFiler()
+  # LotordbFile()
   LotordbServer()
   LotordbServerRunnable(test=True)
   client = LotordbClient('127.0.0.1', 1337)
