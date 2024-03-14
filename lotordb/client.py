e0112f148e4ca31dc54840e867766dd43466cbbf
Nicklas Boman <smurfd@gmail.com>
2024-03-14 21:18:24 +0100

    client cleanup

--- lotordb/client.py
+++ lotordb/client.py
@@ -3,7 +3,7 @@ import threading, socket, ssl
 from lotordb.keys import Keys
 from typing import Union
 
-
+"""
 class ClientCaller(threading.Thread):
   def __init__(self, dbhost, dbport, dbmaster=True, dbnode=0, dbtype=False) -> None:
     threading.Thread.__init__(self, group=None)
@@ -29,33 +29,56 @@ class ClientCaller(threading.Thread):
 
   def receive(self, data):
     self.sock.recv(data)
+"""
 
 
 class ClientRunnable(threading.Thread):
-  def __init__(self, dbtype=False) -> None:
+  def __init__(self, dbhost, dbport, dbmaster=True, dbnode=0, dbtype=False) -> None:
     threading.Thread.__init__(self, group=None)
-    self.client = Union[None, ClientCaller]
+    self.client = Union[None, ClientRunnable]
+    self.event = threading.Event()
+    self.host = dbhost
+    self.port = dbport
+    self.sock = None
     self.type = dbtype
-    self.run()
+    self.thread = threading.Thread()
+    self.start()
 
   def run(self) -> None:
     try:
-      self.client = ClientCaller('127.0.0.1', 1337, dbtype=self.type)
-      self.client.start()
-      self.client.connect()
+      # self.client = ClientCaller('127.0.0.1', 1337, dbtype=self.type)
+      # self.client.start()
+      self.thread.start()
+      self.connect()
       if self.type == 'key':  # key value client
         k = Keys(k='1122', v='abc', s='/tmp')
         print('sending', k.get_key())
-        self.client.send(k.get_key()[0].encode('UTF-8'))
-        self.client.send(k.get_key()[1].encode('UTF-8'))
-        self.client.send(k.get_key()[2].encode('UTF-8'))
+        self.send(k.get_key()[0].encode('UTF-8'))
+        self.send(k.get_key()[1].encode('UTF-8'))
+        self.send(k.get_key()[2].encode('UTF-8'))
       elif self.type == 'db':  # database client
         pass
-      self.client.join()
+      self.thread.join()
     except Exception:
       print('Could not connect to server')
+
+  def connect(self):
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    context.load_verify_locations('.lib/selfsigned.cert')
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    self.sock = context.wrap_socket(sock, server_hostname='localhost')
+    self.sock.connect((self.host, self.port))
+    return self.sock
+
+  def send(self, data):
+    self.sock.send(data)
+
+  def receive(self, data):
+    self.sock.recv(data)
 
 
 if __name__ == '__main__':
   print('Client')
-  client = ClientRunnable(dbtype='key')
+  ClientRunnable('127.0.0.1', 1337, dbtype='key')
+
+  # client = ClientRunnable(dbtype='key')
--- lotordb/test/test_lotordb.py
+++ lotordb/test/test_lotordb.py
@@ -7,7 +7,7 @@ import time
 def test_lotordb() -> None:
   t = time.perf_counter()
   ServerRunnable(test=True, dbtype='key')
-  ClientRunnable(dbtype='key')
+  ClientRunnable('127.0.0.1', 1337, dbtype='key')
   print('time {:.4f}'.format(time.perf_counter() - t))
 
 
