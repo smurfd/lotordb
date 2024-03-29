5aeb735048fc1a0d46a777e2e421e7c023001fd0
Nicklas Boman <smurfd@gmail.com>
2024-03-29 02:36:31 +0100

    timing

--- lotordb/files.py
+++ lotordb/files.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from dataclasses import dataclass, field
 from typing import List, Union, BinaryIO, Tuple
-import struct, gzip, time, threading
+import struct, gzip, time, threading, mmap
 
 # Thinking out loud about how to do a database
 """
@@ -39,6 +39,7 @@ retrieving:
 """
 
 
+# TODO: mmap instead of read/write
 @dataclass
 class DbIndex:
   index: Union[bytes, None] = field(default=b'', init=True)
@@ -63,15 +64,23 @@ class DbData:
   data: Union[bytes, None] = field(default=b'', init=True)
 
 
+# Maby? https://renatocunha.com/2015/11/ctypes-mmap-rwlock/
+# Before mmap : time 6.3838
+# After mmap  : time 6.3694
+# After compress level: time 4.9225
 class Files(threading.Thread):
   def __init__(self, fn) -> None:
     threading.Thread.__init__(self, group=None)
+    self.thread = threading.Thread()
     self.fi: Union[None, BinaryIO] = None
     self.fd: Union[None, BinaryIO] = None
+    self.fimm: Union[None, BinaryIO] = None
+    self.fdmm: Union[None, BinaryIO] = None
     self.fn = (fn + '.dbindex', fn + '.dbdata')
     self.open_index_file(self.fn[0], 'ab+')
     self.open_data_file(self.fn[1], 'ab+')
     self.size = 4048
+    self.thread.start()
     self.start()
 
   def __exit__(self) -> None:
@@ -99,9 +108,18 @@ class Files(threading.Thread):
 
   def init_data(self, index, database, table, relative, row, col, data, dbi) -> Union[DbData, List]:
     packed: List[Union[bytes, None]] = [None] * 7
-    gzd: bytes = gzip.compress(struct.pack('>%dQ' % (len(data)), *data))
     var: List = [index, database, table, relative, row, col]
     ret: List = []
+    t = time.perf_counter()
+    tt = threading.Thread()
+    tt.start()
+    gzd: bytes = gzip.compress(struct.pack('>%dQ' % (len(data)), *data), compresslevel=1)
+    tt.join(timeout=0.1)
+    print('gzip compress time {:.4f}'.format(time.perf_counter() - t))
+    if not struct.unpack('>Q', dbi.seek) and self.fd:
+      dbi.seek = struct.pack('>Q', self.fd.tell())
+    if self.fd:
+      self.fd.seek(struct.unpack('>Q', dbi.seek)[0], 0)
     # Calculate diff between length of gz data, if not divisable with self.size, add 1 to j
     j: int = (len(gzd) // self.size) if not (len(gzd) - ((len(gzd) // self.size) * self.size) > 0) else (len(gzd) // self.size) + 1
     for i in range(j):
@@ -127,7 +145,10 @@ class Files(threading.Thread):
       var: List = [dbd[i].index, dbd[i].database, dbd[i].table, dbd[i].relative, dbd[i].row, dbd[i].col]
       dat += dbd[i].data
       ret.append([struct.unpack('>Q', var[c]) for c in range(6)])
+    tt = threading.Thread()
+    tt.start()
     uncdat = gzip.decompress(dat)
+    tt.join(timeout=0.1)
     return ret, struct.unpack('>%dQ' % (len(uncdat) // 8), uncdat)
 
   def write_index(self, dbi) -> None:
@@ -140,29 +161,57 @@ class Files(threading.Thread):
       [self.fd.write(var[c]) for c in range(7) if self.fd]
 
   def read_index(self) -> Union[DbIndex, None]:
-    self.close_file()
+    # self.close_file()
     self.open_index_file(self.fn[0], 'rb+')
-    return DbIndex(*(self.fi.read([8, 8, 8, 8, 8, 8, 8, 8, 255][c]) for c in range(9) if self.fi))
+    self.fimm = mmap.mmap(self.fi.fileno(), 0, access=mmap.ACCESS_READ)  # type: ignore
+    return DbIndex(*(self.fimm.read([8, 8, 8, 8, 8, 8, 8, 8, 255][c]) for c in range(9) if self.fimm))
 
   def read_data(self, dbi) -> List:
-    self.close_file()
+    # self.close_file()
     self.open_data_file(self.fn[1], 'rb+')
-    return [DbData(*(self.fd.read([8, 8, 8, 8, 8, 8, 4048][c]) for c in range(7) if self.fd)) for i in range(struct.unpack('>Q', dbi.segments)[0])]
+    self.fdmm = mmap.mmap(self.fd.fileno(), 0, access=mmap.ACCESS_READ)  # type: ignore
+    return [
+      DbData(*(self.fdmm.read([8, 8, 8, 8, 8, 8, 4048][c]) for c in range(7) if self.fdmm)) for i in range(struct.unpack('>Q', dbi.segments)[0])
+    ]
 
 
 if __name__ == '__main__':
   print('DB')
+  tot = time.perf_counter()
   t = time.perf_counter()
-  print('time {:.4f}'.format(time.perf_counter() - t))
   f = Files('.lib/db1')
+  print('construct time {:.4f}'.format(time.perf_counter() - t))
+  t = time.perf_counter()
   a = f.init_index(1, 1, 1, 1, 1, 1, 1, 0, '.lib/db1.dbindex')
+  print('init_index time {:.4f}'.format(time.perf_counter() - t))
+  t = time.perf_counter()
   b = f.init_data(1, 1, 1, 1, 1, 1, [123] * 100000025, a)
+  print('init_data time {:.4f}'.format(time.perf_counter() - t))
+  t = time.perf_counter()
   f.get_index(a)
+  print('get_index time {:.4f}'.format(time.perf_counter() - t))
+  t = time.perf_counter()
   d1, d2 = f.get_data(a, b)
-  assert list(d2) == [123] * 100000025
+  print('get_data time {:.4f}'.format(time.perf_counter() - t))
+  # t = time.perf_counter()
+  # assert list(d2) == [123] * 100000025
+  # print('assert time {:.4f}'.format(time.perf_counter() - t))
+  t = time.perf_counter()
   f.write_index(a)
+  print('write_index time {:.4f}'.format(time.perf_counter() - t))
+  t = time.perf_counter()
   f.write_data(a, b)
+  print('write_data time {:.4f}'.format(time.perf_counter() - t))
+  t = time.perf_counter()
   a2 = f.read_index()
+  print('read_index time {:.4f}'.format(time.perf_counter() - t))
+  t = time.perf_counter()
   b2 = f.read_data(a)
+  print('read_data time {:.4f}'.format(time.perf_counter() - t))
+  t = time.perf_counter()
   d3, d4 = f.get_data(a2, b2)
-  print('time {:.4f}'.format(time.perf_counter() - t))
+  print('get_data time {:.4f}'.format(time.perf_counter() - t))
+  # print(f.fd.tell())
+  # print(f.fd.seek(0, io.SEEK_END))
+  # print(f.fd.tell())
+  print('time {:.4f}'.format(time.perf_counter() - tot))
