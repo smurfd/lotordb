9e7b3bdc6523c3ae19d289fff3d0c208c82fbeac
Nicklas Boman <smurfd@gmail.com>
2024-04-01 21:11:54 +0200

    remove db test

--- lotordb/test/test_lotordb.py
+++ lotordb/test/test_lotordb.py
@@ -12,12 +12,12 @@ def test_lotordb_key() -> None:
   print('time {:.4f}'.format(time.perf_counter() - t))
 
 
-def test_lotordb_db() -> None:
-  time.sleep(0.1)
-  t = time.perf_counter()
-  ServerRunnable('127.0.0.1', 7331, test=True, dbtype='db')
-  ClientRunnable('127.0.0.1', 7331, dbtype='db')
-  print('time {:.4f}'.format(time.perf_counter() - t))
+# def test_lotordb_db() -> None:
+#  time.sleep(0.1)
+#  t = time.perf_counter()
+#  ServerRunnable('127.0.0.1', 7331, test=True, dbtype='db')
+#  ClientRunnable('127.0.0.1', 7331, dbtype='db')
+#  print('time {:.4f}'.format(time.perf_counter() - t))
 
 
 if __name__ == '__main__':
