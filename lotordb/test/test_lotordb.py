5e61402055ceeb9dea8e711e5b652e0814ac07ca
Nicklas Boman <smurfd@gmail.com>
2024-04-01 21:21:28 +0200

    test

--- lotordb/test/test_lotordb.py
+++ lotordb/test/test_lotordb.py
@@ -7,17 +7,19 @@ import time
 def test_lotordb_key() -> None:
   time.sleep(0.1)
   t = time.perf_counter()
-  ServerRunnable('127.0.0.1', 7335, test=True, dbtype='key')
-  ClientRunnable('127.0.0.1', 7335, dbtype='key')
+  ServerRunnable('127.0.0.1', 1337, test=True, dbtype='key')
+  time.sleep(0.1)
+  ClientRunnable('127.0.0.1', 1337, dbtype='key')
   print('time {:.4f}'.format(time.perf_counter() - t))
 
 
-# def test_lotordb_db() -> None:
-#  time.sleep(0.1)
-#  t = time.perf_counter()
-#  ServerRunnable('127.0.0.1', 7331, test=True, dbtype='db')
-#  ClientRunnable('127.0.0.1', 7331, dbtype='db')
-#  print('time {:.4f}'.format(time.perf_counter() - t))
+def test_lotordb_db() -> None:
+  time.sleep(0.1)
+  t = time.perf_counter()
+  ServerRunnable('127.0.0.1', 1337, test=True, dbtype='db')
+  time.sleep(0.1)
+  ClientRunnable('127.0.0.1', 1337, dbtype='db')
+  print('time {:.4f}'.format(time.perf_counter() - t))
 
 
 if __name__ == '__main__':
