7019596ae78874357831e8fc9de08aebebd410a3
Nicklas Boman <smurfd@gmail.com>
2024-04-08 01:02:45 +0200

    typing

--- lotordb/hash.py
+++ lotordb/hash.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python3
 import threading, hashlib
+from typing import Union
 
 
 class Hash(threading.Thread):
-  def __init__(self, s):
+  def __init__(self, s) -> None:
     threading.Thread.__init__(self, group=None)
-    self.s = s
-    self.h = None
+    self.s: Union[str, bytes] = s
+    self.h: Union[str, None] = None
     self.start()
 
-  def __exit__(self, exc_type, exc_value, traceback):
+  def __exit__(self, exc_type, exc_value, traceback) -> None:
     self.join()
 
-  def run(self):
+  def run(self) -> None:
     if isinstance(self.s, bytes):
       self.h = hashlib.sha3_512(self.s).hexdigest()
     else:
       self.h = hashlib.sha3_512(self.s.encode('UTF-8')).hexdigest()
 
-  def get(self):
+  def get(self) -> str:
     self.join()
-    return self.h
+    if self.h:
+      return self.h
+    return ''
 
 
 if __name__ == '__main__':
