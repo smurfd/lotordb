c4a7d59b5a91eff0ee14023e920ecfdfa82f8fe2
Nicklas Boman <smurfd@gmail.com>
2024-07-05 22:37:19 +0200

    c backend TODO: add aes and ecdsa

--- lotordb/src/hash.h
+++ lotordb/src/hash.h
@@ -1,7 +1,7 @@
 // Auth: smurfd, 2023 More reading & Borrow/Stolen parts read at the bottom of the file; 2 spacs indent; 120 width    //
 #ifndef LIGHTHASH_H
 #define LIGHTHASH_H 1
-
+#include <stdint.h>
 // Imitate pythons %. -1 % 5 = 4, not -1
 #define MOD(n, m) (((int)n % (int)m) + (int)m) % (int)m
 #define SHA3_BITS 1024 // SHA3-256 = 512, SHA3-512 = 1024 (default)
