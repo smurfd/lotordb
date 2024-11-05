33cf0c0f0d76aada1007d733ae8cfe8067ab15c4
Nicklas Boman <smurfd@gmail.com>
2024-11-05 21:59:29 +0100

    table c backend, rethinking tables tests

--- lotordb/src/tests/tests.c
+++ lotordb/src/tests/tests.c
@@ -14,23 +14,26 @@
 static uint8_t iv1[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
 static uint8_t key1[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f};
 static uint8_t outdec[256] = {0}, outenc[256] = {0}, lain[] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
-void test_ciphers_aes_gcm_text32loop(void) {
+
+uint8_t test_ciphers_aes_gcm_text32loop(void) {
   for (int i = 0; i < 10000; i++) {
     aes_gcm_encrypt(outenc, lain, 32, key1, 32, iv1, 32);
     aes_gcm_decrypt(outdec, outenc, sizeof(outenc), key1, 32, iv1, 32);
     for (int i = 0; i < 32; i++) assert(lain[i] == outdec[i]);
   }
+  return 1;
 }
 
-void test_ciphers_aes_gcm_text32(void) {
+uint8_t test_ciphers_aes_gcm_text32(void) {
   aes_gcm_encrypt(outenc, lain, 32, key1, 32, iv1, 32);
   aes_gcm_decrypt(outdec, outenc, sizeof(outenc), key1, 32, iv1, 32);
   for (int i = 0; i < 32; i++) assert(lain[i] == outdec[i]);
+  return 1;
 }
 
 //
 // Generate a keypair & shared key then print it (test / demo)
-void test_genkeys(void) {
+uint8_t test_genkeys(void) {
   head h1, h2;
   h1.g = u64rnd(); h1.p = u64rnd(); h2.g = u64rnd(); h2.p = u64rnd();
   u64 c = 123456, d = 1, e = 1;
@@ -44,19 +47,21 @@ void test_genkeys(void) {
   handler_cryptography(d, k2, &e);
   printf("Before:  0x%.20llx\nEncrypt: 0x%.20llx\nDecrypt: 0x%.20llx\n",c,d,e);
   assert(c == e);
+  return 1;
 }
 
-void test_keys_verify(void) {
+uint8_t test_keys_verify(void) {
   uint8_t sig[BYTES * 2],  pubkey[BYTES + 1],  sec[BYTES], privkey[BYTES], h[BYTES] = {0};
   assert(keys_make(pubkey, privkey));
   assert(keys_secr(pubkey, privkey, sec));
   assert(keys_sign(privkey, h, sig));
   assert(keys_vrfy(pubkey, h, sig));
   assert(!keys_vrfy(privkey, h, sig)); // assert failure
+  return 1;
 }
 
 // TODO: this needs to be wayyy simpler. More helper functions
-void test_db_table(void) {
+uint8_t test_db_table(void) {
   // Define database table structure
   struct prstest {
     u64 age;
@@ -91,8 +96,13 @@ void test_db_table(void) {
       // Search for age == 666
       if (((struct prstest*)((struct ctx*)c)->structure)->age == 666) {
         printf("found\n");
+        // Free memory
+        if (c->structure != NULL) free(c->structure);
+        if (header != NULL) free(header);
+        if (bin != NULL) free(bin);
+        if (c != NULL) free(c);
         fclose(read_ptr);
-        return;
+        return 1;
       }
     }
   }
@@ -102,18 +112,20 @@ void test_db_table(void) {
   if (bin != NULL) free(bin);
   if (c != NULL) free(c);
   fclose(read_ptr);
+  return 0;
 }
 
 int main(int argc, char** argv) {
+  int ret = 1;
   if (argc == 1) {
     printf("\"[o.o]\"              testing ....              \"[o.o]\"\n\n");
-    printf("lotordb test\n");
-    test_genkeys();
-    test_keys_verify();
-    test_ciphers_aes_gcm_text32();
-    test_ciphers_aes_gcm_text32loop();
-    test_db_table();
-    printf("\nOK\n");
+    ret &= test_genkeys();
+    ret &= test_keys_verify();
+    ret &= test_ciphers_aes_gcm_text32();
+    ret &= test_ciphers_aes_gcm_text32loop();
+    ret &= test_db_table();
+    if (ret) printf("\nOK\n");
+    else printf("\nNot OK\n");
   } else {
     if (strcmp(argv[1], "server") == 0) {
       connection c = server_init("127.0.0.1", "9998", usage(argv[2], argc, "server"));
