18b2f115aa2ad7b92c3f3452d64fc5df36cf01e5
Nicklas Boman <smurfd@gmail.com>
2024-11-08 22:58:42 +0100

    table c backend, rethinking tables examples include cleanup

--- lotordb/src/examples/tables_example_client.h
+++ lotordb/src/examples/tables_example_client.h
@@ -1,12 +1,4 @@
 #ifndef TABLES_CLIENT_H
 #define TABLES_CLIENT_H 1
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/socket.h>
-#include "../db_keystore.h"
-
 void table_client(int sock);
 #endif
--- lotordb/src/examples/tables_example_server.c
+++ lotordb/src/examples/tables_example_server.c
@@ -1,9 +1,6 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <sys/socket.h>
 #include "../db_tables.h"
 
 void table_server(int sock) {
