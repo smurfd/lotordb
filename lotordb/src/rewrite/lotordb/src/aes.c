cfa852a04a3cabd68da3c1c3363d78326563e2df
Nicklas Boman <smurfd@gmail.com>
2024-12-03 22:40:40 +0100

    c backend rewrite more aes

--- lotordb/src/rewrite/lotordb/src/aes.c
+++ lotordb/src/rewrite/lotordb/src/aes.c
@@ -3,6 +3,44 @@
 #include <stdio.h>
 #include "aes.h"
 
+// TODO: Figure out later how to generate these
+static const uint8_t sbox[256] = {
+  0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
+  0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
+  0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
+  0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
+  0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
+  0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
+  0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
+  0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
+  0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
+  0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
+  0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
+  0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
+  0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
+  0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+  0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
+  0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16};
+
+static const uint8_t reverse_sbox[256] = {
+  0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
+  0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
+  0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
+  0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
+  0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92,
+  0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84,
+  0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06,
+  0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b,
+  0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73,
+  0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e,
+  0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b,
+  0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4,
+  0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
+  0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
+  0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
+  0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d};
+uint8_t state[4][4] = {0};
+
 // TODO: Fix always have 1st argument as return value if needed
 static inline unsigned long long str_to_bin(const char *s) {
   unsigned long long i = 0;
@@ -194,6 +232,236 @@ inline static unsigned long long bin_to_long(uint8_t *bin) {
    }
 */
 
+/*
+// good read:
+//   https://engineering.purdue.edu/kak/compsec/NewLectures/Lecture8.pdf
+//   https://www.cse.wustl.edu/~jain/cse571-11/ftp/l_05aes.pdf
+//   https://ie.u-ryukyu.ac.jp/~wada/design04/spec_e.html
+//   https://blog.0x7d0.dev/education/how-aes-is-implemented/
+
+
+// AES Pseudocode https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.197-upd1.pdf
+// 128, 192, 256 (Nk = 4, 6, 8)
+func CIPHER(in, Nr, w) {
+  state = in
+  state = ADDROUNDKEY(state, w[0..3])
+  for round from 1 to Nr - 1 do
+    state = SUBBYTES(state)
+    state = SHIFTROWS(state)
+    state = MIXCOLUMNS(state)
+    satet = ADDROUNDKEY(state, w[4*round..4*round+3])
+  end for
+  state = SUBBYTES(state)
+  state = SHIFTROWS(state)
+  state = ADDROUNDKEY(state, w[4*Nr..4*Nr+3])
+  return state
+}
+
+func ADDROUNDKEY(state, key) {
+  // memcpy to key so it contains right frame of key
+  for i from 0 to 4 do
+    for j from 0 to 4 do
+      state[j][i] ^= key[(i * 4) + j] 
+}
+
+func SUBBYTES(state) {
+  for i from 0 to 4 do
+    for j from 0 to 4 do
+      state[j][i] = SBOX[state[j][i]]
+}
+
+func INVSUBBYTES(state) {
+  for i from 0 to 4 do
+    for j from 0 to 4 do
+      state[j][i] = SBOXINV[state[j][i]]
+}
+
+func SHIFTROWS(state) {
+  // skip first row
+  // rotate rows to left, number of steps = row
+  temp = state[0][1]
+  state[0][1] = state[1][1]
+  state[1][1] = state[2][1]
+  state[2][1] = state[3][1]
+  state[3][1] = temp
+
+  temp = state[0][2]
+  state[0][2] = state[2][2]
+  state[2][2] = temp
+  temp = state[1][2]
+  state[1][2] = state[3][2]
+  state[3][2] = temp
+
+  temp = state[0][3]
+  state[0][3] = state[3][3]
+  state[3][3] = state[2][3]
+  state[2][3] = state[1][3]
+  state[1][3] = temp
+}
+
+func INVSHIFTROWS(state) {
+  // skip first row
+  // rotate rows to right, number of steps = row
+  temp = state[3][1]
+  state[3][1] = state[2][1]
+  state[2][1] = state[1][1]
+  state[1][1] = state[0][1]
+  state[0][1] = temp
+
+  temp = state[0][2]
+  state[0][2] = state[2][2]
+  state[2][2] = temp
+  temp = state[1][2]
+  state[1][2] = state[3][2]
+  state[3][2] = temp
+
+  temp = state[0][3]
+  state[0][3] = state[1][3]
+  state[1][3] = state[2][3]
+  state[2][3] = state[3][3]
+  state[3][3] = temp
+}
+
+func times(x) {
+  return ((x<<1) ^ (((x>>7) & 1) * 0x1b))
+}
+
+func MIXCOLUMNS(state) {
+  for i from 0 to 4 do
+    state0   = state[i][0];
+    statecol = state[i][0] ^ state[i][1] ^ state[i][2] ^ state[i][3]
+    statesav = times(state[i][0] ^ state[i][1])
+    state[i][0] ^= statesav ^ statecol
+
+    statesav = times(state[i][1] ^ state[i][2])
+    state[i][1] ^= statesav ^ statecol
+
+    statesav = times(state[i][2] ^ state[i][3])
+    state[i][2] ^= statesav ^ statecol
+
+    statesav = times(state[i][3] ^ state0)
+    state[i][3] ^= statesav ^ statecol
+}
+
+func KEYEXPANSION(key) {
+  i = 0
+  while i <= Nk - 1 do
+    w[i] = key[4*i..4*i+3]
+    i += 1
+  end while
+  while i <= 4 * Nr + 3 do
+    temp = w[i - 1]
+    if i mod Nk == 0 {
+      temp = SUBWORD(ROTWORD(temp)) ^ Rcon[i/Nk]
+    } else if Nk > 6 and i mod Nk == 4 {
+      temp = SUBWORD(temp)
+    }
+    w[i] = w[i - Nk] ^ temp
+    i += 1
+  end while
+  return w
+}
+
+func INVCIPHER(in, Nr, w) {
+  state = in
+  state = ADDROUNDKEY(state, w[4*Nr..4*Nr+3])
+  for round = Nr - 1 d >= 1 {
+    state = INVSHIFTROWS(state)
+    state = INVSUBBYTES(state)
+    state = ADDROUNDKEY(state, w[4*round..4*round+3])
+    state = INVMIXCOLUMNS(state)
+  }
+  state = INVSHIFTROWS(state)
+  state = INVSUBBYTES(state)
+  state = ADDROUNDKEY(state, w[0..3])
+  return state
+}
+
+func EQINVCIPHER(in, Nr, dw) {
+  state = in
+  state = ADDROUNDKEY(state, dw[4*Nr..4*Nr+3])
+  for round = Nr - 1 d >= 1 {
+    state = INVSUBBYTES(state)
+    state = INVSHIFTROWS(state)
+    state = INVMIXCOLUMNS(state)
+    state = ADDROUNDKEY(state, dw[4*round..4*round+3])
+  }
+  state = INVSUBBYTES(state)
+  state = INVSHIFTROWS(state)
+  state = ADDROUNDKEY(state, dw[0..3])
+  return state
+}
+
+func KEYEXPANSIONEIC(key) {
+  i = 0
+  while i <= Nk - 1 do
+    w[i] = key[4*i..4*i+3]
+    dw[i] = w[i]
+    i += 1
+  end while
+  while i <= 4 * Nr + 3 do
+    temp = w[i - 1]
+    if i mod Nk == 0 {
+      temp = SUBWORD(ROTWORD(temp)) ^ Rcon[i/Nk]
+    } else if Nk > 6 and i mod Nk == 4 {
+      temp = SUBWORD(temp)
+    }
+    w[i] = w[i - Nk] ^ temp
+    dw[i] = w[i]
+    i += 1
+  end while
+  for round = 1 <= Nr - 1 {
+    i = 4 * round
+    dw[i..i+3] = INVMIXCOLUMNS(dw[i..i+3])
+  }
+  return dw
+}
+
+// https://github.com/m3y54m/aes-in-c?tab=readme-ov-file#the-rijndael-key-schedule
+// https://en.wikipedia.org/wiki/Rijndael_S-box
+#define ROTL8(x,shift) ((uint8_t) ((x) << (shift)) | ((x) >> (8 - (shift))))
+
+void initialize_aes_sbox(uint8_t sbox[256]) {
+  uint8_t p = 1, q = 1;
+  
+  // loop invariant: p * q == 1 in the Galois field
+  do {
+    // multiply p by 3
+    p = p ^ (p << 1) ^ (p & 0x80 ? 0x1B : 0);
+
+    // divide q by 3 (equals multiplication by 0xf6)
+    q ^= q << 1;
+    q ^= q << 2;
+    q ^= q << 4;
+    q ^= q & 0x80 ? 0x09 : 0;
+
+    // compute the affine transformation
+    uint8_t xformed = q ^ ROTL8(q, 1) ^ ROTL8(q, 2) ^ ROTL8(q, 3) ^ ROTL8(q, 4);
+
+    sbox[p] = xformed ^ 0x63;
+  } while (p != 1);
+
+  // 0 is a special case since it has no inverse
+  sbox[0] = 0x63;
+}
+
+
+static void ShiftRows(AES_BYTE *b, int Nb, char *t)
+{
+  AES_BYTE *temp = alloca(AES_WS*Nb);
+  int i, j;
+
+  memcpy(temp, b, AES_WS*Nb);
+
+  for (i = 0; i < AES_WS; i++) {
+    for (j = 0; j < Nb; j++) {
+      b[j*AES_WS + i] = temp[(j + t[i] + Nb) % Nb * AES_WS + i];
+    }
+  }
+}
+
+*/
+
 uint8_t *right_pad_to_multiple_of_16_bytes(uint8_t *input, int len) {
   while(len++ % 16 != 0) {
     input[len] = 0;
@@ -242,12 +510,13 @@ void xorarr(uint8_t *X, uint8_t *Y, uint8_t *r) {
 // 6.4 for GHASH
 // In effect, the GHASH function calculates: (X1*Hm) ^ (X2*Hm-1) ^ ... ^ (Xm-1*H2) ^ (Xm*H)
 uint8_t **ghash(uint8_t **X, uint8_t **H, int m) { // X must be 128*m length
-  uint8_t Y[128][128] = {0}, RET[128][128];
+  uint8_t Y[128][128] = {0}, RET[128][128]; // TODO: we assume m=128
   for (int i = 1; i < m; i++) {
+    // LONG2BIN(X[i])? LONG2BIN(H[m-(i-i)])?
     memcpy(&RET[i], mul(X[i], H[m-(i-1)]), 128);
   }
   for (int i = 1; i < m; i++) {
-    xorarr(RET[i], RET[i+1], &Y[i]);
+    xorarr(RET[i-1], RET[i], &Y[i]);
   }
   return Y;
 }
@@ -306,7 +575,7 @@ uint8_t *AES_CTR(uint8_t *key, uint8_t *initial_counter_block, uint8_t *in, u64 
     block[2] = little_endian_uint32(*(&block[2]+1));
     block[3] = little_endian_uint32(*(&block[3]+1));
 
-    u64 keystream_blocklen = 16; // TODO: fix
+    u64 keystream_blocklen = 16; // TODO: fix correct length
     if (inlen < keystream_blocklen) todo = inlen;
     else todo = keystream_blocklen;//min(inlen, key_generating_key);//min(bytelen(in), bytelen(keystream_block));
     for (int j = 0; j < todo; j++) {
