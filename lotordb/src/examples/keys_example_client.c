c9d49ff5526b946fd34dccbd8e76c472e6b605ae
Nicklas Boman <smurfd@gmail.com>
2025-01-10 18:30:50 +0100

    c backend rewrite, fix warning in key example

--- lotordb/src/examples/keys_example_client.c
+++ lotordb/src/examples/keys_example_client.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include "../db_keystore.h"
 
 void key_client(int sock) {
+  char key[256] = {""}, value[256] = {""}, store[256] = {""};
   kvsh *k = (kvsh*)malloc(sizeof(struct kvsh));
-  key_set(k, "0002", "testvalue", "/tmp");
+  memcpy(key, "0002", 4);
+  memcpy(value, "testvalue", 9);
+  memcpy(store, "/tmp", 4);
+  key_set(k, key, value, store);
   key_write(k);
   key_del(k);
   key_send(sock, k);
